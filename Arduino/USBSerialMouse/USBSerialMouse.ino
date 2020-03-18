9710fbef73dbaa6677b247ffcb7fd0a895c24e8a
willson5127 <willson5127@hotmail.com>
2020-03-18 15:18:35 +0800

    Arduino mouse contrul command.

--- Arduino/USBSerialMouse/USBSerialMouse.ino
+++ Arduino/USBSerialMouse/USBSerialMouse.ino
@@ -1,6 +1,9 @@
 #include <SoftwareSerial.h>   // 引用程式庫
+#include <Mouse.h>
 
 String st;
+int xDistance = 0, yDistance = 0;
+int M = 0;
  
 void setup() 
 {
@@ -10,12 +13,69 @@ void setup()
  
 void loop() 
 {
+  if(M == 1)
+  {
+    Serial.println("Please insert Xpos!");    
+  }
+  else if(M == 2)
+  {
+    Serial.println("Please insert Ypos!");
+  }
   if (Serial.available()) 
   {   
     char val = Serial.read();
 
     if(val == '\r')
     {
+      if(M == 0)
+      {
+        if (st == "Move*")
+        {
+          M = 1;
+        }
+        else if (st == "ClickLeftBTN*") 
+        {
+          Mouse.press(MOUSE_LEFT);
+          delay(10);
+          Mouse.release(MOUSE_LEFT);     
+        }
+        else if (st == "ClickRightBTN*") 
+        {
+          Mouse.press(MOUSE_RIGHT);
+          delay(10);
+          Mouse.release(MOUSE_RIGHT);     
+        }
+        else if (st == "PressLeftBTN*") 
+        {
+          Mouse.press(MOUSE_LEFT);     
+        }
+        else if (st == "releaseLeftBTN*")
+        {
+          Mouse.release(MOUSE_LEFT);  
+        }
+        else if (st == "PressRightBTN*") 
+        {
+          Mouse.press(MOUSE_RIGHT);     
+        }
+        else if (st == "releaseRightBTN*")
+        {
+          Mouse.release(MOUSE_RIGHT);  
+        }
+      }
+      else if(M == 1)
+      {        
+        xDistance = st.toInt();
+        M = 2;
+      }
+      else if(M == 2)
+      {
+        yDistance = st.toInt();
+        if ((xDistance != 0) || (yDistance != 0)) 
+        {
+          Mouse.move(xDistance, yDistance, 0);
+          M = 0;
+        }
+      }     
       Serial.println(st);
     }
     else if(val == '\n')
